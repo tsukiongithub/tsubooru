70043cd1a967b2f6e0adca44af64890279d86cb5
tsukiongithub <lucasmoon0@gmail.com>
2023-03-06 17:15:27 +0100

    added out

---
+++ _next/static/7CjiaXUPtl8xsb9clTgBq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/c16184b3-69f261b79ad85b76.js","static/chunks/512-056fb8cc4c769332.js","static/chunks/pages/index-c10e26d5d5dfc055.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
